d749aff72dd3ea02efdb7dca2ecb44346231e924
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-11-08 16:18:29 +0300

    Initial commit

---
+++ src/main/java/com/demo/cucumber_spring/CucumberSpringApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cucumber_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberSpringApplication.class, args);
+	}
+
+}
